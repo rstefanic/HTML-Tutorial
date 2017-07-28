287c0cc05e63982c50bcc7881d98a40d63b443a5
rstefanic <rstefanic72@gmail.com>
2017-07-28 17:42:09 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+class InputOutputPair {
+    constructor(inputElement, outputElement) {
+	this.inputElement = inputElement;
+	this.outputElement = outputElement;
+	this.addKeyUpTextListener();
+    }
+
+    addKeyUpTextListener() {
+	this.inputElement.onkeyup = () => {
+	    updateText(this.inputElement,
+		       this.outputElement);
+	};
+    }
+}
+
+function updateText(inputText, outputText) {
+    outputText.innerHTML = inputText.value;
+}
+
+function generatePairs(amountOfPairs) {
+    for(let i = 0; i < amountOfPairs; i++) {
+	new InputOutputPair(document.getElementById("ei" + i),
+	                    document.getElementById("eo" + i));
+    }
+}
+
+generatePairs(1);
